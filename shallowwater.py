9832c30e63fac1c3f64c897dc874cb5ac8e38d47
Matthew Rocklin <mrocklin@gmail.com>
2011-11-09 12:36:19 -0600

    cleaned demo
    removed possibility of anisotropic grid
    moved conservative evolution to "unused code" section
    removed call to global variables in demo
    changed default numsteps to 3000

--- shallowwater.py
+++ shallowwater.py
@@ -5,12 +5,12 @@ box_size = 1.
 
 
 # Initial Conditions
-n,m = 100,100
-u = zeros((n,m)) # velocity in x direction
-v = zeros((n,m)) # velocity in y direction
+n = 100
+u = zeros((n,n)) # velocity in x direction
+v = zeros((n,n)) # velocity in y direction
 
-eta = ones((n,m)) # pressure deviation (like height)
-x,y = mgrid[:n,:m]
+eta = ones((n,n)) # pressure deviation (like height)
+x,y = mgrid[:n,:n]
 droplet_x, droplet_y = 50, 50
 rr = (x-droplet_x)**2 + (y-droplet_y)**2
 eta[rr<10**2] = 1.1 # add a perturbation in pressure surface
@@ -22,7 +22,7 @@ dt = grid_spacing / 100.
 
 def spatial_derivative(A, axis=0):
     """
-    Compute derivative using balanced finite differences
+    Compute derivative of array A using balanced finite differences
     Axis specifies direction of spatial derivative (d/dx or d/dy)
 
     dA[i] =  A[i+1] - A[i-1]   / 2
@@ -40,18 +40,6 @@ def d_dx(A):
 def d_dy(A):
     return spatial_derivative(A,0)
 
-def d_dt(eta, u, v, g):
-    """
-    http://en.wikipedia.org/wiki/Shallow_water_equations#Conservative_form
-    """
-    for x in [eta, u, v]: # type check
-        assert isinstance(x, ndarray) and not isinstance(x, matrix)
-
-    deta_dt = -d_dx(eta*u) -d_dy(eta*v)
-    du_dt = (deta_dt*u - d_dx(eta*u**2 + 1./2*g*eta**2) - d_dy(eta*u*v)) / eta
-    dv_dt = (deta_dt*v - d_dx(eta*u*v) - d_dy(eta*v**2 + 1./2*g*eta**2)) / eta
-
-    return deta_dt, du_dt, dv_dt
 
 def d_dt(eta, u, v, g, b=0):
     """
@@ -95,8 +83,7 @@ def evolveEuler(eta, u, v, g, dt=dt):
 
         yield eta, u, v
 
-def demo(numsteps=1000):
-    global eta, u, v, g, dt
+def demo(eta=eta, u=u, v=v, g=g, dt=dt, numsteps=3000):
     trajectory = evolveEuler(eta, u, v, g, dt)
 
     # Figure with initial conditions
@@ -112,4 +99,21 @@ def demo(numsteps=1000):
     figure(); title('After %d steps ; time=%f'%(numsteps, numsteps*dt))
     eta, u, v = trajectory.next()
     imshow(eta); colorbar()
+
+#=========================================
+# Unused but possibly relevant code
+#=========================================
+
+def d_dt_conservative(eta, u, v, g):
+    """
+    http://en.wikipedia.org/wiki/Shallow_water_equations#Conservative_form
+    """
+    for x in [eta, u, v]: # type check
+        assert isinstance(x, ndarray) and not isinstance(x, matrix)
+
+    deta_dt = -d_dx(eta*u) -d_dy(eta*v)
+    du_dt = (deta_dt*u - d_dx(eta*u**2 + 1./2*g*eta**2) - d_dy(eta*u*v)) / eta
+    dv_dt = (deta_dt*v - d_dx(eta*u*v) - d_dy(eta*v**2 + 1./2*g*eta**2)) / eta
+
+    return deta_dt, du_dt, dv_dt
 
